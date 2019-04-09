d81f991e2a939729775a5f456b0d65cdb9715d59
Peter Galanthay <peter@scifire.de>
2019-04-09 17:16:03 +0200

    First draft of adaptation for Proemion (#1)

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,36 +3,55 @@
 # This class installs and configures a Symantec BackupExec agent on Linux
 #
 class backupexec (
-    $pkgname = $backupexec::params::pkgname,
-) inherits backupexec::params {
+  $be_server,
+  $be_userpw,
+  $pkgname = $backupexec::params::pkgname,
+  ) inherits backupexec::params {
+
   group { 'beoper':
     ensure => present,
   }
   user { 'beuser':
-    ensure  => present,
-    gid     => '0',
-    groups  => 'beoper',
-    require => Group['beoper']
+    ensure   => present,
+    system   => true,
+    gid      => '0',
+    groups   => 'beoper',
+    password => $be_userpw,
+    require  => Group['beoper']
   }
 
   package { $pkgname:
-    ensure  => present,
+    ensure  => latest,
     require => User['beuser'],
   }
 
-  file { '/etc/VRTSralus/ralus.cfg':
-    ensure  => file,
-    owner   => 'beuser',
-    group   => 'beoper',
-    mode    => '0644',
-    content => template('backupexec/ralus.cfg.erb'),
-    require => Package[$backupexec::params::pkgname],
+  file_line { 'backupexec_agent_directory':
+    ensure => present,
+    path   => '/etc/VRTSralus/ralus.cfg',
+    line   => "Software\\Symantec\\Backup Exec For Windows\\Backup Exec\\Engine\\Agents\\Agent Directory List_1=$be_server",
+    match  => '^Software\\Symantec\\Backup Exec For Windows\\Backup Exec\\Engine\\Agents\\Agent Directory List_1=.*',
+    require => Package[$pkgname],
+  }
+  file_line { 'backupexec_adverticement_port':
+    ensure => present,
+    path   => '/etc/VRTSralus/ralus.cfg',
+    line   => "Software\\Symantec\\Backup Exec For Windows\\Agent Browser\\TcpIp\\AdvertisementPort=6101",
+    match  => '^Software\\Symantec\\Backup Exec For Windows\\Agent Browser\\TcpIp\\AdvertisementPort=',
+    require => Package[$pkgname],
+  }
+
+
+  file { '/opt/VRTSralus/bin/VRTSralus.init':
+    ensure  => 'file',
+    mode    => '0755',
+    require => Package[$pkgname],
   }
 
   file { '/etc/init.d/VRTSralus.init':
-    ensure => 'link',
-    target => '/opt/VRTSralus/bin/VRTSralus.init',
-    before => Service['VRTSralus.init'],
+    ensure  => 'link',
+    target  => '/opt/VRTSralus/bin/VRTSralus.init',
+    before  => Service['VRTSralus.init'],
+    require => Package[$pkgname],
   }
 
   service { 'VRTSralus.init':
@@ -41,7 +60,7 @@ class backupexec (
     hasstatus  => false,
     hasrestart => true,
     pattern    => '/opt/VRTSralus/bin/beremote',
-    require    => Package[$backupexec::params::pkgname],
+    require    => Package[$pkgname],
   }
 
   file { '/opt/VRTSralus/data':
@@ -49,6 +68,6 @@ class backupexec (
     owner   => 'beuser',
     group   => 'beoper',
     mode    => '0770',
-    require => Package[$backupexec::params::pkgname],
+    require => Package[$pkgname],
   }
 }
